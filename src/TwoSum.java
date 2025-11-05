8d2ccf591170c947b7aeb538e2f2655fe06800dc
Zeenath Nisha Shajahan <zeenathnisha2411@gmail.com>
2025-11-05 12:16:34 +0530

    Create TwoSum.java

---
+++ src/TwoSum.java
@@ -0,0 +1,23 @@
+package day5;
+import java.util.HashMap;
+
+public class TwoSum {
+    public static int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i}; // found the pair
+            }
+            map.put(nums[i], i); // store value and its index
+        }
+        return new int[]{-1, -1}; // if not found
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = twoSum(nums, target);
+        System.out.println("Indices: " + result[0] + ", " + result[1]);
+    }
+}
